71f73308cd71cb14d62d1a01808c2dc45e45acd2
BhargavManiar <bhargav22478@gmail.com>
2020-08-09 20:44:40 +0100

    Changed code to optimise for printing less information on to the standard I/O. Used state machine technique to indicate a change in the led status.
    Changed counter resets to be in one if statement.

--- MorseCodeReceiver/MorseCodeReceiver.ino
+++ MorseCodeReceiver/MorseCodeReceiver.ino
@@ -2,7 +2,10 @@ int LDR = A0;
 int baudrate = 9600;
 int counterOne = 0; // The time the led is on 
 int counterTwo = 0; // The time the led is off
-bool high = false;
+
+bool currentState;
+bool priorState;
+
 
 void setup() {
   pinMode(LDR, INPUT);
@@ -14,27 +17,34 @@ void loop() {
   int LDR_out = analogRead(LDR);
   
   if(LDR_out >= 1000) { // Dark 
+    currentState = false;
+    
     counterOne ++;
     Serial.print("LED Off Counter One: ");
     Serial.println(counterOne);
     counterTwo = 0;
     
   } else {            // Light
+    currentState = true;
+    
     counterTwo ++;
     Serial.print("LED On Counter Two: ");
     Serial.println(counterTwo); 
     counterOne = 0;
-    
+  }
+
+  if(currentState != priorStatus)
+  {
+    Serial.println("Change");
+    priorState = currentState;
+    // Add print of counter two here 
   }
   
-  if(counterTwo >= 50) {
+  if(counterTwo >= 50 || counterOne >= 50) {
     counterTwo = 0;
-    Serial.println("Seems that the transmission has finished");
-  }
-  
-  if(counterOne >= 50) {
     counterOne = 0;
     Serial.println("Seems that the transmission has finished");
   }
+  
   delay(100);
 }
