1d4d695c09448f4930900fd84aba1c2b1e4cd24d
BhargavManiar <bhargav22478@gmail.com>
2020-08-12 22:00:54 +0100

    Removed redundant variables. Note that in this version it is noticible that autocorreclation is needed. There are instances when the receiver does not obtain any information, despite the transmitter working

--- MorseCodeReceiver/MorseCodeReceiver.ino
+++ MorseCodeReceiver/MorseCodeReceiver.ino
@@ -1,13 +1,9 @@
 int LDR = A0;
 int baudrate = 9600;
-int counterOne = 0; // The time the led is on 
-int counterTwo = 0; // The time the led is off
 int changeOfStateCounter = 0; // Hold the count of the change
 
 bool currentState;
 bool priorState;
-bool restartLoop; 
-
 
 void setup() {
   pinMode(LDR, INPUT);
@@ -20,12 +16,8 @@ void loop() {
   
   if(LDR_out >= 1000) { // Dark 
     currentState = false;
-    counterOne ++;
-    counterTwo = 0; 
   } else {            // Light
     currentState = true;
-    counterTwo ++;
-    counterOne = 0;
   }
 
   if(currentState != priorState) {
@@ -33,13 +25,9 @@ void loop() {
 
     if(changeOfStateCounter == 9 && currentState == false){
       Serial.println(".");
-      //Serial.println(counterOne);
-      //Serial.println(counterTwo);
     }
     if(changeOfStateCounter == 19 && currentState == false){
       Serial.println("-");
-      //Serial.println(counterOne);
-      //Serial.println(counterTwo);
     }
     changeOfStateCounter = 0; // Reset the counter
     priorState = currentState; // Make the prior state the current one
