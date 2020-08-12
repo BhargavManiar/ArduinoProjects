200ca3fc1072ecaa7ae53ea164bcf144ad62cfba
BhargavManiar <bhargav22478@gmail.com>
2020-08-12 20:25:38 +0100

    Changed code to report the counts for a change to occur

--- MorseCodeReceiver/MorseCodeReceiver.ino
+++ MorseCodeReceiver/MorseCodeReceiver.ino
@@ -2,6 +2,7 @@ int LDR = A0;
 int baudrate = 9600;
 int counterOne = 0; // The time the led is on 
 int counterTwo = 0; // The time the led is off
+int changeOfStateCounter = 0; // Hold the count of the change
 
 bool currentState;
 bool priorState;
@@ -18,26 +19,31 @@ void loop() {
   
   if(LDR_out >= 1000) { // Dark 
     currentState = false;
-    
     counterOne ++;
-    Serial.print("LED Off Counter One: ");
-    Serial.println(counterOne);
+    //Serial.print("LED Off Counter One: ");
+    //Serial.println(counterOne);
     counterTwo = 0;
     
   } else {            // Light
     currentState = true;
-    
     counterTwo ++;
-    Serial.print("LED On Counter Two: ");
-    Serial.println(counterTwo); 
+    //Serial.print("LED On Counter Two: ");
+    //Serial.println(counterTwo); 
     counterOne = 0;
   }
 
   if(currentState != priorState)
   {
-    Serial.println("Change");
+    //Serial.println("Change");
     priorState = currentState;
     // Add print of counter two here 
+    //Serial.println(currentState);
+    Serial.println(changeOfStateCounter);
+    changeOfStateCounter = 0; // Reset the counter
+  }
+  else // I.e the same state
+  {
+    changeOfStateCounter++;
   }
   
   if(counterTwo >= 50 || counterOne >= 50) {
