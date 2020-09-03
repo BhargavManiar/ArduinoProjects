f70c9eed30f16674e67254cc3c9c079c27b824e9
BhargavManiar <bhargav22478@gmail.com>
2020-09-03 14:06:39 +0100

    Added pin configuration and completed setup method

--- OneDigitSevenSegmentDisplay/OneDigitSevenSegmentDisplay.ino
+++ OneDigitSevenSegmentDisplay/OneDigitSevenSegmentDisplay.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
 
+// Pin configuration
+int dot = 1; // For displaying segment "."
+int a = 2;   // For displaying segment "a"
+int b = 3;   // For displaying segment "b"
+int c = 4;   // For displaying segment "c"
+int d = 5;   // For displaying segment "d"
+int e = 6;   // For displaying segment "e"
+int f = 7;   // For displaying segment "f"
+int g = 8;   // For displaying segment "g"
+
+void setup() {
+  pinMode(a,OUTPUT);  // A
+  pinMode(b,OUTPUT);  // B
+  pinMode(c,OUTPUT);  // C
+  pinMode(d,OUTPUT);  // D
+  pinMode(e,OUTPUT);  // E
+  pinMode(f,OUTPUT);  // F
+  pinMode(g,OUTPUT);  // G
+  pinMode(dot,OUTPUT);// Dot
+  
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
