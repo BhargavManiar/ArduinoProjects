278fc224f1ef9d7fe56d561512a5bff8161fbfe1
BhargavManiar <bhargav22478@gmail.com>
2020-09-21 21:06:57 +0100

    Added code to stop the motor after a period of time

--- DCMotor/MotorTest/MotorTest.ino
+++ DCMotor/MotorTest/MotorTest.ino
@@ -5,5 +5,8 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(motor, HIGH);
+  digitalWrite(motor, HIGH);  // Start Motor
+  delay(10000);               // Wait 10 seconds
+  digitalWrite(motor,LOW);    // Stop Motor
+  delay(10000);               // Wait 10 seconds
 }
