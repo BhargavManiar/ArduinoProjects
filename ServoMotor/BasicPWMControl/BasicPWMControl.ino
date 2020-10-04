46b7374d7b1f27d5872a2501aa3176601d9c891d
BhargavManiar <bhargav22478@gmail.com>
2020-10-04 19:45:51 +0100

    Added code to control the motor to pot the arm into 0, 90 and 180 degree positions

--- ServoMotor/BasicPWMControl/BasicPWMControl.ino
+++ ServoMotor/BasicPWMControl/BasicPWMControl.ino
@@ -9,6 +9,16 @@ void setup() {
 }
 
 void loop() {
-  servoMotor.write(90); // Move the arm
-  delay(1000);       // Wait one second
+  servoMotor.write(0);    // Move the arm to the 0 degree position
+  delay(1000);            // Wait one second
+  
+  servoMotor.write(90);   // Move the arm to the 90 degree position
+  delay(1000);            // Wait one second
+
+  servoMotor.write(180);  // Move the arm to the 180 degree position
+  delay(2000);            // Wait two seconds
+
+  
+  
+  
 }
