725f14d1a71d9fd8098b9dfda24456aebd48ecf1
BhargavManiar <bhargav22478@gmail.com>
2020-10-04 19:36:51 +0100

    Completed setup

--- ServoMotor/BasicPWMControl/BasicPWMControl.ino
+++ ServoMotor/BasicPWMControl/BasicPWMControl.ino
@@ -2,7 +2,8 @@
 
 Servo servoMotor; // Servo motor object
 int servoPin = 5; // The pin the servo was connected to
+int angle = 0;    // This is the angle from the start point
 
 void setup() {
-  
+  servoMotor.attach(servoPin); // We can write to this pin.  
 }
