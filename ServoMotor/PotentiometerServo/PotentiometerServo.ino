0441e54cc3edd9a22ec18b0925aef2c8bb2ea827
BhargavManiar <bhargav22478@gmail.com>
2020-10-04 21:57:00 +0100

    Commented outputs to improve responsiveness of the servo motor with the potentiometer

--- ServoMotor/PotentiometerServo/PotentiometerServo.ino
+++ ServoMotor/PotentiometerServo/PotentiometerServo.ino
@@ -11,11 +11,18 @@ int angle = 0; // The position of the servo arm
 
 void setup() {
   servoMotor.attach(servoPin);
+  //Serial.begin(9600);
 }
 
 void loop() {
   potentiometerValue = analogRead(potentiometerPin); // Read the analogue pin
   angle = map(potentiometerValue, 0, 1023, 0, 180); // map input to numbers between 0 and 180
-
   servoMotor.write(angle); // Write the servo based on the mapped value
+  /*
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potentiometerValue);
+  Serial.print("Angle Value: ");
+  Serial.println(angle);
+  */
+  
 }
