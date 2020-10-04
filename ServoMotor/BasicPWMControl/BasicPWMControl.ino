d491995f4d5b34ed9e129681584c02fc8ea45d8b
BhargavManiar <bhargav22478@gmail.com>
2020-10-04 19:35:24 +0100

    Added headfile for servo motor and created some of the basic variables

--- ServoMotor/BasicPWMControl/BasicPWMControl.ino
+++ ServoMotor/BasicPWMControl/BasicPWMControl.ino
@@ -1,3 +1,7 @@
+#include <Servo.h> // Library for controlling the servo motor
+
+Servo servoMotor; // Servo motor object
+int servoPin = 5; // The pin the servo was connected to
 
 void setup() {
   
