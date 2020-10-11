324b4e0225e9e55299222fea696bad64fd76b6a0
BhargavManiar <bhargav22478@gmail.com>
2020-10-11 22:02:27 +0100

    Completed main loop code

--- Infrared/IR_Remote/IR_Remote.ino
+++ Infrared/IR_Remote/IR_Remote.ino
@@ -1,7 +1,7 @@
 #include <IRremote.h>
 
 const int RECV_PIN = 7; // The pin that is connected to the reciever 
-IRrecv irrecv(RECV_PIN):
+IRrecv irrecv(RECV_PIN);
 decode_results results;
 const int redPin = 10;  // Controls one LED
 const int greenPin = 11;// Controls other LED
@@ -14,5 +14,20 @@ void setup() {
 }
 
 void loop() {
-  
+  if(irrecv.decode(&results)) {
+    
+    switch(results.value) {
+      case 0xFF38C7: // This is button 5 on the remote. 
+      digitalWrite(redPin, HIGH);   // Turn the LED on
+      delay(2000);  // Wait 2 seconds 
+    }
+
+    switch(results.value) {
+      case 0xFF18E7 // This is button 2 on the remote. 
+      digitalWrite(greenPin, HIGH): // Turn the LED on
+      delay(2000);  // Wait 2 seconds
+    }
+
+    irrecv.resume();
+  }
 }
