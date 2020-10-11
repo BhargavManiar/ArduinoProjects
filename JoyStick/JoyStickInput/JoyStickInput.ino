a69c31df3be5b9647e27422df4cc2b0dc1869c5a
BhargavManiar <bhargav22478@gmail.com>
2020-10-11 21:11:13 +0100

    Made changes to the code to have two differnt types of output, one more human legible and one for CSV.
    Got data from the joy stick and data is present in JoyStick Data Analysis 1 file.

--- JoyStick/JoyStickInput/JoyStickInput.ino
+++ JoyStick/JoyStickInput/JoyStickInput.ino
@@ -4,13 +4,32 @@ const int Switch_Pin = 2; // Digital 2 pin connected to output
 const int X_pin = 0;      // Analogue 0 pin connected to X output
 const int Y_pin = 1;      // Analogue 1 pin connected to Y output
 
+bool outputType = false;  // Normal = false, CSV = true
 void setup() {
   pinMode(Switch_Pin,INPUT);      // Configure Switch pin as input
   digitalWrite(Switch_Pin, HIGH); // Set the Switch pin as high 
   Serial.begin(115200);           // 12 Times the normal baud rate 
+  if(outputType) {
+    Serial.println("Switch, X Axis, Y Axis");
+  }
 }
 
 void loop() {
+  if(outputType) {
+    CSVOutput();
+  } else {
+    regularOutput();
+  }
+}
+void CSVOutput() {
+  Serial.print(digitalRead(Switch_Pin));
+  Serial.print(",");
+  Serial.print(analogRead(X_pin));
+  Serial.print(",");
+  Serial.println(analogRead(Y_pin));
+  delay(125);
+}
+void regularOutput() {
   Serial.print("Switch: ");
   Serial.print(digitalRead(Switch_Pin));
   Serial.print("\n");
