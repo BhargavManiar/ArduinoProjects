50b01e3d60c110f3b1e7b32f5284e0906353c50a
BhargavManiar <bhargav22478@gmail.com>
2020-10-11 21:58:41 +0100

    Completed setup function

--- Infrared/IR_Remote/IR_Remote.ino
+++ Infrared/IR_Remote/IR_Remote.ino
@@ -3,9 +3,16 @@
 const int RECV_PIN = 7; // The pin that is connected to the reciever 
 IRrecv irrecv(RECV_PIN):
 decode_results results;
-const int redPin = 10;
-const int greenPin = 11;
+const int redPin = 10;  // Controls one LED
+const int greenPin = 11;// Controls other LED
 
 void setup() {
+  irrecv.enableIRIN(); 
+  irrecv.blink13(true);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+}
+
+void loop() {
   
 }
