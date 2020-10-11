165a419ed002cfd849cd534194d6b259c9d85fd4
BhargavManiar <bhargav22478@gmail.com>
2020-10-11 23:23:30 +0100

    Changed code to better control the LEDs in terms of turning them off.

--- Infrared/IR_Remote/IR_Remote.ino
+++ Infrared/IR_Remote/IR_Remote.ino
@@ -20,13 +20,24 @@ void loop() {
       case 0xFF38C7: // This is button 5 on the remote. 
         digitalWrite(redPin, HIGH);   // Turn the LED on
         delay(2000);  // Wait 2 seconds
-      case  
+    }
+
+    switch(results.value) {
+      case 0xFF5AA5:  // This is button 6
+        digitalWrite(redPin, LOW);    // Turn the LED off
+        delay(2000);
     }
 
     switch(results.value) {
       case 0xFF18E7: // This is button 2 on the remote. 
-      digitalWrite(greenPin, HIGH); // Turn the LED on
-      delay(2000);  // Wait 2 seconds
+        digitalWrite(greenPin, HIGH); // Turn the LED on
+        delay(2000);  // Wait 2 seconds
+    }
+
+    switch(results.value) {
+      case 0xFF7A85: // This is button 3 on the remote
+        digitalWrite(greenPin, LOW); // Turn the LED off
+        delay(2000);      
     }
 
     irrecv.resume();
