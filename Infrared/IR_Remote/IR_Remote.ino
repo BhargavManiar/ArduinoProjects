13ba9f803886179272e942d5aedf9d0a3471c976
BhargavManiar <bhargav22478@gmail.com>
2020-10-11 21:55:03 +0100

    Added the buzzer files and added the Infared files

---
+++ Infrared/IR_Remote/IR_Remote.ino
@@ -0,0 +1,11 @@
+#include <IRremote.h>
+
+const int RECV_PIN = 7; // The pin that is connected to the reciever 
+IRrecv irrecv(RECV_PIN):
+decode_results results;
+const int redPin = 10;
+const int greenPin = 11;
+
+void setup() {
+  
+}
